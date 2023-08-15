881134e8abdaca462d08a06155ed1ab2cfc1ed8b
Welson Oktario <welsonoktario@yahoo.com>
2023-08-15 12:05:09 +0700

    fix(db): Fix db connection URI

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,13 +4,19 @@ import mongoose from 'mongoose'
 configDotenv({ path: `.env.${process.env.NODE_ENV}` })
 
 const { DB_URL, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
+const user = encodeURIComponent(DB_USER)
+const password = encodeURIComponent(DB_PASSWORD)
 const dsn =
   DB_USER && DB_PASSWORD
-    ? `${DB_URL}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
-    : `${DB_URL}://${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`
+    ? `${DB_URL}://${user}:${password}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    : `${DB_URL}://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 console.log(dsn)
 
 export const connectDb = async () =>
   await mongoose.connect(dsn, {
-    connectTimeoutMS: 10000,
+    connectTimeoutMS: 0,
+    retryWrites: true,
+    writeConcern: {
+      w: 'majority',
+    },
   })
